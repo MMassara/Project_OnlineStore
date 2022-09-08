d33dadd1fa4390211eaea29f84e89761e363b3f8
HailMuffins <rafaelsm123@outlook.com>
2022-09-08 16:58:00 -0300

    Função getCategories completada

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,24 @@
 export async function getCategories() {
   // Implemente aqui
   try {
-    const response = await fetch(
-      'https://api.mercadolibre.com/sites/MLB/categories',
-    );
-    const data = await response.json();
+    const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+    const data = response.json();
     return data;
   } catch (error) {
-    console.log(`Algo deu errado : ${error}`);
+    console.log(`${error}`);
   }
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const category = categoryId || '$CATEGORY_ID';
+  const realQuery = query || '$QUERY';
+  try {
+    const productsFromCategory = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${category}&q=${realQuery}}`);
+    const items = await productsFromCategory.json();
+    return items;
+  } catch (error) {
+    console.log('erro');
+  }
 }
 
 export async function getProductById() {
