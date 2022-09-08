a5741b1ec1b47ee623ff0c3a563b27a95d3237d0
Pedroscsilva <pedroscsilva2001@gmail.com>
2022-09-08 17:59:50 -0300

    parte req 5

--- src/Components/PageInicial.jsx
+++ src/Components/PageInicial.jsx
@@ -1,20 +1,58 @@
 import React from 'react';
+import { getProductsFromCategoryAndQuery } from '../services/api';
+import ProductCard from './ProductCard';
 
 class PageInicial extends React.Component {
   state = {
     listaInicial: '',
+    searchQuery: '',
+    renderItems: [],
+    renderData: false,
   };
 
+  handleClick = async () => {
+    //faz o fetch da função 'getProductsFromCategoryAndQuery':
+    const { searchQuery } = this.state;
+    const promise = await getProductsFromCategoryAndQuery(null, searchQuery);
+    const data = promise.results;
+    const infoToRender = data.length === 0 ? 'Nenhum produto foi encontrado' : data;
+    this.setState({
+      renderItems: infoToRender,
+      renderData: true,
+    });
+  };
+
+  handleChange = ({ target }) => {
+    //salva no estado o input de pesquisa do usuário do usuário
+    const { value } = target;
+    this.setState({
+      searchQuery: value,
+    });
+  };
+
+  //parei na hora de renderizar os itens para a página inicial. Criei um componente ProductCard para isso.
+
   render() {
-    const { listaInicial } = this.state;
+    const { listaInicial, renderItems, renderData } = this.state;
     return (
       <div>
-        <input type="text" />
+        <input
+          type="text"
+          data-testid="query-input"
+          onChange={this.handleChange}
+        />
+        <button
+          type="button"
+          data-testid="query-button"
+          onClick={this.handleClick}
+        >
+          Pesquisar
+        </button>
         {listaInicial.length === 0 ? (
           <h3 data-testid="home-initial-message">
             Digite algum termo de pesquisa ou escolha uma categoria.
           </h3>
-        ) : null }
+        ) : null} 
       </div>
     );
   }
---
+++ src/Components/ProductCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ProductCard extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Oi</h3>
+      </div>
+    );
+  }
+}
+
+export default ProductCard;
