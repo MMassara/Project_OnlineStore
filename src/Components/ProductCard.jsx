27cd676fb59cb6980f8d4d312b8b4bbabd73b2b5
Matheus Massara <matheus.massara@hotmail.com>
2022-09-09 19:02:31 -0300

    Finalizado requisito 08

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -2,13 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class ProductCard extends React.Component {
+  componentDidMount() {
+    if (localStorage.getItem('produto') === null) {
+      localStorage.setItem('produto', JSON.stringify([]));
+    }
+  }
+
+  addToCart = () => {
+    const {
+      itemObj: { thumbnail, title, price },
+    } = this.props;
+    const allItems = JSON.parse(localStorage.getItem('produto'));
+    allItems.push({ price, title, thumbnail, quantity: 1 });
+    localStorage.setItem('produto', JSON.stringify(allItems));
+  };
+
   render() {
-    const { itemObj: { thumbnail, title, price } } = this.props;
+    const {
+      itemObj: { thumbnail, title, price },
+    } = this.props;
+
     return (
       <>
         <img src={ thumbnail } alt={ title } data-testid="product" />
         <h3>{title}</h3>
         <h4>{price}</h4>
+        <button
+          type="button"
+          onClick={ this.addToCart }
+          data-testid="product-add-to-cart"
+        >
+          Adicionar ao carrinho!
+        </button>
       </>
     );
   }
